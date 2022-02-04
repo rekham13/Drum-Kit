bf9f6acd15a7d68bc105d60b9eba13e147d44bc1
rekha <rekhamanjunath37162@gmail.com>
2022-02-04 16:25:13 +0530

    css js changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
+
 for (var i = 0; i < noOfDrumButtons;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("I got clicked");
+    var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
     });
 }
 
