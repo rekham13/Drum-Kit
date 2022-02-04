fff3f51b9bf0edf1a89d916a97e83f9e7ea2e580
rekha <rekhamanjunath37162@gmail.com>
2022-02-04 13:59:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    });
+}
+
+
+
